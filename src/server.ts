8f16091a9eda7ba15eec0707a0dc9c3d5e134fac
diegobressler <diegobressler@gmail.com>
2021-06-21 20:28:22 -0300

    sql

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 const token = 'b85e8ee4854850a0c0cdccb971434298513a155c';
 
- 
- var options = {
+
+/* var options = {
     setHeaders: function (res, path, stat) {
       res.set('Authorization', token)
     }
@@ -12,10 +12,21 @@ const token = 'b85e8ee4854850a0c0cdccb971434298513a155c';
   }
   
 app.get('https://api.brasil.io/v1/dataset/socios-brasil/empresas/data/?cnpj=92343375000172', ( request, response) => {
+    request.header('Authorization'): token;
+
     response.set('Authorization',token)
     return response.send(" Ola nwwl");
+});*/
+
+app.get("/test",( request, response) => {
+    response.set('Authorization',token)
+    return response.send(request.headers);
 });
 
 app.post("/test-post",( request, response) => {
-    return response.send(" Ola nwwl");
+    return response.send("tes");
 });
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+  })
